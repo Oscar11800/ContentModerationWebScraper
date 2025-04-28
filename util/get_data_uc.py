6db00cb1ae774250b2501fa5d0d3ee85d55f3d85
Oscar Chen <oscarc@uchicago.edu>
2025-04-28 09:44:13 -0500

    server code working

--- util/get_data_uc.py
+++ util/get_data_uc.py
@@ -30,10 +30,10 @@ class UC_Scraper:
         options.add_argument(f"user-agent={UserAgent().random}")  # Random user-agent
         options.add_argument("--disable-blink-features=AutomationControlled")  # Prevent bot detection
         options.add_argument("--blink-settings=imagesEnabled=false")  # disables images
-        options.add_argument("--no-sandbox")  
+        options.add_argument("--no-sandbox")
+        options.add_argument("--headless")
         options.add_argument("--disable-dev-shm-usage")
 
-      
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 
@@ -48,7 +48,7 @@ class UC_Scraper:
                 WebDriverWait(self.driver, timeout=15).until(
                     lambda driver: driver.execute_script("return document.readyState") == "complete"
                 )
-                time.sleep(0.5) # make sure JS loads
+                # time.sleep(0.5) # make sure JS loads
                 page_source = self.driver.page_source
                 text = Soup(page_source, features='lxml').get_text()
             except (TimeoutException, WebDriverException, InvalidSessionIdException) as e:
